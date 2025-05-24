73409f4bbb43c32c1369c7676fed7bed6d5e7996
Who <thisisandrew0110@gmail.com>
2025-05-25 05:40:01 +0800

    added tuple_cat

--- headers/tuplex_utils.hpp
+++ headers/tuplex_utils.hpp
@@ -110,7 +110,7 @@ namespace tuplex
                 return static_cast<const volatile data_t&&>(static_cast<const volatile Tp&&>(t).data());
             }
         };
-    } // detail
+    } // namespace detail
 
     /* ------------------------------------------------------------------------------------------------ */
     template <typename...>
@@ -147,5 +147,106 @@ namespace tuplex
     constexpr tuplex::tuple<std::unwrap_ref_decay_t<Args>...> make_tuple(Args&&... args)
     {
         return tuplex::tuple<std::unwrap_ref_decay_t<Args>...>{ std::forward<Args>(args)... };
+    }
+
+    /* ------------------------------------------------------------------------------------------------ */
+    template <typename...>
+    struct cat_types;
+
+    template <typename... Ts>
+    struct cat_types<tuplex::tuple<Ts...>> : detail::type_identity<tuplex::tuple<Ts...>> {};
+
+    template <typename... Ts1, typename... Ts2, typename... Other>
+    struct cat_types<tuplex::tuple<Ts1...>, tuplex::tuple<Ts2...>, Other...> : 
+        detail::type_identity<typename cat_types<tuplex::tuple<Ts1..., Ts2...>, Other...>::type> {};
+
+    template <typename... Tps>
+    using cat_types_t = typename cat_types<Tps...>::type;
+
+    /* ------------------------------------------------------------------------------------------------ */
+    namespace detail
+    {
+        template <typename AccumulatorTs,
+                  typename CurrentIndices,
+                  typename NextIndices>
+        struct tuple_cat_impl;
+
+        template <typename... AccumulatorTs,
+                  std::size_t... CurrentIndices,
+                  std::size_t... NextIndices>
+        struct tuple_cat_impl<tuplex::tuple<AccumulatorTs...>,
+                              std::index_sequence<CurrentIndices...>,
+                              std::index_sequence<NextIndices...>>
+        {
+            template <typename ZeroTuple>
+            constexpr auto operator()(tuplex::tuple<AccumulatorTs...>&& accumulator_ts,
+                                      ZeroTuple&& zero_tuple) noexcept
+            {
+                return tuplex::forward_as_tuple(
+                    tuplex::get<CurrentIndices>(std::move(accumulator_ts))...,
+                    tuplex::get<NextIndices>(std::forward<ZeroTuple>(zero_tuple))...
+                );
+            } 
+
+            template <typename FirstTuple, typename SecondTuple, typename... Other>
+            constexpr auto operator()(tuplex::tuple<AccumulatorTs...>&& accumulator_ts,
+                                      FirstTuple&& fst_tuple,
+                                      SecondTuple&& snd_tuple,
+                                      Other&&... other) noexcept
+            {
+                auto new_tuple{
+                    tuplex::forward_as_tuple(
+                        tuplex::get<CurrentIndices>(std::move(accumulator_ts))...,
+                        tuplex::get<NextIndices>(std::forward<FirstTuple>(fst_tuple))...
+                    )
+                };
+
+                using PureSecond = std::remove_cvref_t<SecondTuple>;
+
+                using new_t = decltype(new_tuple);
+                
+                using current_indices = std::make_index_sequence<tuplex::tuple_size_v<new_t>>;
+                using next_indices = std::make_index_sequence<tuplex::tuple_size_v<PureSecond>>;
+
+                using next_t = tuple_cat_impl<new_t,
+                                              current_indices,
+                                              next_indices>;
+
+                return next_t{}(std::move(new_tuple), 
+                                std::forward<SecondTuple>(snd_tuple),
+                                std::forward<Other>(other)...);
+            }
+        };
+
+        template <typename Ret, typename Tp, std::size_t... Indices>
+        constexpr Ret tuple_cat_ret_wrapper(Tp&& tp, std::index_sequence<Indices...>) noexcept
+        {
+            return Ret { 
+                tuplex::get<Indices>(std::forward<Tp>(tp))...
+            };
+        }
+    } // namespace detail
+
+    template <typename FirstTuple, typename... Other>
+    constexpr auto tuple_cat(FirstTuple&& fst_tuple, Other&&... other) noexcept
+    {
+        using T1 = std::remove_cvref_t<FirstTuple>;
+        using Ret = cat_types_t<T1, std::remove_cvref_t<Other>...>;
+
+        using current_indices = std::make_index_sequence<tuplex::tuple_size_v<T1>>;
+        using RetIndicies = std::make_index_sequence<tuplex::tuple_size_v<Ret>>;
+
+        using CatExecutor = detail::tuple_cat_impl<tuplex::tuple<>,
+                                                   std::index_sequence<>,
+                                                   current_indices>;
+
+        return detail::tuple_cat_ret_wrapper<Ret>(
+            CatExecutor{}(
+                tuplex::tuple<>{},
+                std::forward<FirstTuple>(fst_tuple),
+                std::forward<Other>(other)...
+            ),
+            RetIndicies{}
+        );
     }
 };
